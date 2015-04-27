caac8ee8f100367e5945f8b8c5d94c92bd69d2ac
haru-a8n <haru.a8n@gmail.com>
2015-04-27 00:03:19 -0500

    PyCharm ignore imports

--- haru.py
+++ haru.py
@@ -1,11 +1,14 @@
 import comtypes
 
 from comtypes import *
+# noinspection PyUnresolvedReferences
 from comtypes.client import *
 
 comtypes.client.GetModule('UIAutomationCore.dll')
+# noinspection PyUnresolvedReferences
 from comtypes.gen.UIAutomationClient import *
 
+# noinspection PyProtectedMember,PyUnresolvedReferences
 uia = CoCreateInstance(CUIAutomation._reg_clsid_,
                        interface=IUIAutomation,
                        clsctx=CLSCTX_INPROC_SERVER)
