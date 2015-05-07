cc07fa562bbdd5b96db677bd509e3a261fc4219b
haru-a8n <haru.a8n@gmail.com>
2015-05-06 21:40:30 -0700

    UIA root in a function

--- haru.py
+++ haru.py
@@ -1,18 +1,17 @@
-import comtypes
+def get_uia_root():
+    import comtypes
+    import comtypes.client
 
-from comtypes import *
-# noinspection PyUnresolvedReferences
-from comtypes.client import *
+    comtypes.client.GetModule('UIAutomationCore.dll')
 
-comtypes.client.GetModule('UIAutomationCore.dll')
-# noinspection PyUnresolvedReferences
-from comtypes.gen.UIAutomationClient import *
+    # noinspection PyProtectedMember,PyUnresolvedReferences
+    uia = comtypes.CoCreateInstance(comtypes.gen.UIAutomationClient.CUIAutomation._reg_clsid_,
+                                    interface=comtypes.gen.UIAutomationClient.IUIAutomation,
+                                    clsctx=comtypes.CLSCTX_INPROC_SERVER)
 
-# noinspection PyProtectedMember,PyUnresolvedReferences
-uia = CoCreateInstance(CUIAutomation._reg_clsid_,
-                       interface=IUIAutomation,
-                       clsctx=CLSCTX_INPROC_SERVER)
+    desktop_element = uia.getRootElement()
+    print(desktop_element.currentName)
+    comtypes.CoUninitialize()
 
-desktop_element = uia.getRootElement()
-print(desktop_element.currentName)
-CoUninitialize()
+if __name__ == '__main__':
+    get_uia_root()
