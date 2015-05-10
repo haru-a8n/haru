e3cc7ed4fb5cd3c791bfc9104de16e27c80673c4
haru-a8n <haru.a8n@gmail.com>
2015-05-09 20:33:53 -0700

    start process

--- haru.py
+++ haru.py
@@ -2,12 +2,90 @@ import comtypes
 import comtypes.client
 import subprocess
 
+trace = True
+
+
+class Logger(object):
+    def __init__(self, trace):
+        self.trace = trace
+
+    def __call__(self, fn):
+        if not self.trace:
+            return fn
+
+        from functools import wraps
+
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            print('>--Entering %s' % fn.__name__)
+
+            out = fn(*args, **kwargs)
+
+            print('<--Exiting %s' % fn.__name__)
+            # Return the return value
+            return out
+        return wrapper
+
+
+class CWindow(object):
+    def __init__(self, trace=False, attr=None, parent=None, ae_init=False):
+        """
+        trace(True): Show log entry to most methods
+        attr: attribute name used when called from __attr__
+        parent: parent object
+        ae_init(True): Initialization of Automation Element done at the subclass
+        """
+        assert not (attr is None)
+        assert not (parent is None)
+
+        self.parent = parent
+
+        self.trace = trace
+        super(CWindow, self).__init__()
+
+        if ae_init:
+            pass
+        else:
+            # This will try to match by name
+            ae = self.name_best_match(ae=parent.element, string_match=attr)
+
+            if ae:
+                self.element = ae
+            else:
+                #This is when creating object specifically called for a specific window
+                windows = ['window','statusbar']
+                if str(attr).lower() in windows and ae == None:
+                    #This information gets used in __call__
+                    self.element = self.parent.element
+                else:
+                    self.element = None
+
+    def name_best_match(self, ae=None, string_match=''):
+        if self.trace:
+            print '>>%s@%s@%s'%(self.__class__,sys._getframe(0).f_code.co_name,string_match)
+
+        assert ae, 'AE object None'
+
+        ae = swa.TreeWalker.ControlViewWalker.GetFirstChild( ae )
+        while ae:
+            print '-'*10
+            print 'Name: ',ae.Current.Name
+            print 'LocalizedControlType:',ae.Current.LocalizedControlType
+            print 'ClassName: ',ae.Current.ClassName
+            if self.BestMatch(ae.Current.Name, string_match ):
+                break
+            ae = swa.TreeWalker.ControlViewWalker.GetNextSibling( ae )
+
+        #Don't need to assert of None, we need to check using other mechanism
+        return ae
+
 
 class Robot(object):
     def __init__(self):
         super(Robot, self).__init__()
         self.proc = None
 
+    @Logger(trace=trace)
     def start(self, args):
         """
         Start an  application
